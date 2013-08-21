a87dd45c83d84e843566ef83885b672970e39945
Miguel Mendes Ruiz <migmruiz@gmail.com>
2013-08-21 20:20:01 -0300

    TicketFieldSpec com getters

--- src/main/java/com/taxis99/zendesk/model/TicketFieldSpec.java
+++ src/main/java/com/taxis99/zendesk/model/TicketFieldSpec.java
@@ -2,13 +2,32 @@ package com.taxis99.zendesk.model;
 
 import java.util.List;
 
-public class TicketFieldSpec {
-  int id;
-  String title;
-  List<TicketFieldOption> customFieldOptions;
-}
+import com.google.common.collect.ImmutableList;
 
-class TicketFieldOption {
-  String name;
-  String value;
+public class TicketFieldSpec {
+  private int id;
+  private String title;
+  private List<TicketFieldOption> customFieldOptions;
+
+  public int getId() {
+    return id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public List<TicketFieldOption> getCustomFieldsOptions() {
+    return ImmutableList.copyOf(customFieldOptions);
+  }
+
+  public static class TicketFieldOption {
+    private String name;
+    private String value;
+
+    public String getName() {
+      return name;
+    }
+    public String getValue() {
+      return value;
+    }
+  }
 }
