335ada994121f938baa8daa8b8ca1e30c2923685
Filipe Guedes <fgs.guedes@gmail.com>
2013-08-30 22:07:50 -0300

    Removed bind to Gson.class because it was causing conflicts with binding from mailchimp-java-api

--- src/main/java/com/taxis99/zendesk/config/ZendeskConfigModule.java
+++ src/main/java/com/taxis99/zendesk/config/ZendeskConfigModule.java
@@ -1,6 +1,5 @@
 package com.taxis99.zendesk.config;
 
-import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
@@ -9,16 +8,5 @@ public class ZendeskConfigModule extends AbstractModule {
     bind(ZendeskConfig.class)
       .annotatedWith(Names.named("Authorized"))
         .to(ZendeskConfigFromProperties.class);
-    /// .to(ZendeskConfigImpl.class);
-    bind(Gson.class).toInstance(GsonInstanceHolder.getGsonInstance());
   }
 }
-
-//class ZendeskConfigImpl implements ZendeskConfig {
-//  @Override public String getAuth() {
-//    return "user@domain.com/token:AuTh_T0K3N-Fr0M_Z3nDesK";
-//  }
-//  @Override public String getSubdomain() {
-//    return "subdomain";
-//  }
-//}
